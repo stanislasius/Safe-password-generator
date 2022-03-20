824fb4069972e02421a5da4449c29cd28af8e6c2
S.Uslisty <touchar2@yandex.ru>
2022-03-20 18:58:29 +0300

    Предварительная стабильная версия

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+___version___ = 0.1
+
 # import target modules
 from math import ceil
 from random import sample, shuffle
-from string import ascii_lowercase, ascii_uppercase, digits, punctuation
-
+from string import ascii_lowercase, ascii_uppercase, digits
 
 
 # init variables and their values
@@ -10,36 +11,44 @@ password, password_list =  str(), list()
 punctuation = "!#$%&()*/_"
 
 # Init all functions you need
-def correct_password_number(password_numbers):                       #check entered values, must be only digits 
+def correct_password_number(password_numbers):
+    """Функция проверки корректности введённых данных для количества сгенерированных паролей""" 
     if password_numbers.isdigit() is False:
         password_numbers = input('Вводить нужно ТОЛЬКО ЦЕЛЫЕ ЧИСЛА, так что введите целое число: ')
-        correct_password_number(password_numbers)
+        return correct_password_number(password_numbers)
     else:
         return int(password_numbers)
 
-
-def correct_password_lenght(password_length):                        #check entered values, must be only digits and length less than 8
+def correct_password_lenght(password_length):
+    """Функция проверки корректности введённых данных для длины пароля"""
     if password_length.isdigit() is False or int(password_length) < 8:
-        password_length = input(f'Указаны неверные данные.\nДолжно быть введено только целое число и оно не должно быть меньше 8.\n Введите число: ')
-        correct_password_lenght(password_length)
+        new_password = input(f'Указаны неверные данные.\nДолжно быть введено только целое число и оно не должно быть меньше 8.\nВведите число: ')
+        return correct_password_lenght(new_password)
     elif password_length.isdigit() is True and int(password_length) >= 8:
         return int(password_length)
 
+def start_program():
+    """ Основной модуль программы, где создаются пароли """
 
-# start main program
+    password_numbers = correct_password_number(input('Введите необходимое количество сгенерированных паролей: '))
+    print()
+        
+    password_length = correct_password_lenght(input('Введите длину(кол-во символов) одного пароля(не менее 8): '))
+    print()        
+
+    for i in range(1, password_numbers + 1):
+        password = sample(digits, ceil(password_length / 4)) + sample(ascii_lowercase, ceil(password_length / 4)) + sample(ascii_uppercase, ceil(password_length / 4)) + sample(punctuation, ceil(password_length / 4))
+        shuffle(password)
+        password_list.append(''.join(password[0:password_length]))
+        del password
+        
+    return print(*password_list, sep='\n')
+    
+#Main    
 print('Вы запустили программу генерации паролей.')
 
-password_numbers = correct_password_number(input('Введите необходимое количество сгенерированных паролей: '))
-print()
-    
-password_length = correct_password_lenght(input('Введите длину(кол-во символов) одного пароля(не менее 8): '))
-print()        
+if input('Сгенерировать пароль(-и)? ').lower() == 'да':
+    start_program()
+else:
+    exit('Либо было введено "Нет", либо иные данные. Программа завершает свюо работу.')
 
-for i in range(1, password_numbers + 1):
-    password = sample(digits, ceil(password_length / 4)) + sample(ascii_lowercase, ceil(password_length / 2)) + sample(ascii_uppercase, ceil(password_length / 2)) + sample(punctuation, ceil(password_length / 4))
-    shuffle(password)
-    password_list.append(''.join(password[0:password_length + 1]))
-    del password
-    
-print(*password_list, sep='\n')
-    
